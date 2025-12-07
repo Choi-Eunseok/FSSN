58e5ecbd7a7621a71fe174c1d76d7267751a2af4
Choi-Eunseok <ted12333@gmail.com>
2025-12-07 12:22:50 +0900

    Complete Server HelloGrpcService

--- GrpcDemo.Server/Program.cs
+++ GrpcDemo.Server/Program.cs
@@ -2,12 +2,10 @@ using GrpcDemo.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<HelloGrpcService>();
 app.MapGrpcService<BidirectionalService>();
 app.MapGrpcService<ClientStreamingService>();
--- GrpcDemo.Server/Services/HelloGrpcService.cs
+++ GrpcDemo.Server/Services/HelloGrpcService.cs
@@ -1,6 +1,14 @@
 namespace GrpcDemo.Server.Services;
+using Grpc.Core;
 
-public class HelloGrpcService
+public class HelloGrpcService : MyService.MyServiceBase
 {
-    
+    public override Task<MyNumber> MyFunction(MyNumber request, ServerCallContext context)
+    {
+        var input = request.Value;
+        return Task.FromResult(new MyNumber
+        {
+            Value = input * input
+        });
+    }
 }
