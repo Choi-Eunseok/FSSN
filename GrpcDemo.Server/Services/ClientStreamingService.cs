10fa3bdb9ac1b2e074d41837ca6f7d2bc19f8bb7
Choi-Eunseok <ted12333@gmail.com>
2025-12-07 15:06:22 +0900

    Complete server ClientStreamingService

--- GrpcDemo.Server/Services/ClientStreamingService.cs
+++ GrpcDemo.Server/Services/ClientStreamingService.cs
@@ -1,6 +1,31 @@
-namespace GrpcDemo.Server.Services;
+using Grpc.Core;
+using Clientstreaming;
+using GrpcDemo.Server.Logging;
 
-public class ClientStreamingService
+namespace GrpcDemo.Server.Services
 {
-    
+    public class ClientStreamingService : ClientStreaming.ClientStreamingBase
+    {
+        public override async Task<Number> GetServerResponse(
+            IAsyncStreamReader<Message> requestStream,
+            ServerCallContext context)
+        {
+            LogManager.Log(source: "ClientStreaming", direction: "Info", message: "Server processing gRPC client-streaming.");
+
+            var count = 0;
+
+            await foreach (var msg in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                count++;
+
+                LogManager.Log(source: "ClientStreaming", direction: "Recv", message: $"#{count} 받은 메시지: \"{msg.Message_}\"");
+            }
+
+            var response = new Number { Value = count };
+
+            LogManager.Log(source: "ClientStreaming", direction: "Send", message: $"총 메시지 개수 = {count}");
+
+            return response;
+        }
+    }
 }
